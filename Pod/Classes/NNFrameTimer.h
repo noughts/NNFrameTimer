5463a6c6e835aae63b0f288f773fd51529fa4ddb
Koichi Yamamoto <noughts@gmail.com>
2015-06-22 14:02:24 +0900

    a

---
+++ Pod/Classes/NNFrameTimer.h
@@ -0,0 +1,13 @@
+//
+//  NNFrameTimer.h
+//  Pods
+//
+//  Created by noughts on 2015/06/22.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NNFrameTimer : NSObject
+
+@end
---
+++ Pod/Classes/NNFrameTimer.m
@@ -0,0 +1,13 @@
+//
+//  NNFrameTimer.m
+//  Pods
+//
+//  Created by noughts on 2015/06/22.
+//
+//
+
+#import "NNFrameTimer.h"
+
+@implementation NNFrameTimer
+
+@end
